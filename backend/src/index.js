ef4a3da175cc9477ab6d6abd13ab89192a687ee2
Kanika <97448286+malikkanika9@users.noreply.github.com>
2022-08-02 17:53:28 +0530

    iniatial

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const connect=require("./configs/db")
+const userController=require("./controllers/user.ontroller")
+const{register,login}=require("./controllers/auth.controller")
+const app=express();
+app.use(express.json());
+app.use("/users", userController)
+app.post("/register",register)
+app.post("/login",login)
+app.listen(5000,async()=>{
+try{
+
+    await connect();
+    console.log("listening on port 5000")
+}
+catch(err){
+
+    console.log(err.message)
+}
+
+});
